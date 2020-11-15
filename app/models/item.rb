39e298caae47aae40ec0d068c8dee69a24971291
isaki0317 <matsuoisaki.0317@gmail.com>
2020-11-15 03:47:21 +0000

    ordered_items model create association complete

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,7 +1,8 @@
 class Item < ApplicationRecord
-  
-  has_many :orders
+
+  has_many :orders, through: :ordered_items
   has_many :cart_items
+  has_many :ordered_items
   belongs_to :genre
-  
+
 end
---
+++ app/models/ordered_item.rb
@@ -0,0 +1,6 @@
+class OrderedItem < ApplicationRecord
+  
+  belongs_to :item
+  belongs_to :order
+  
+end
