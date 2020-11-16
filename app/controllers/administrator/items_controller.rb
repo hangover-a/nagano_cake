8234255ee185ab66ec80bd8002400a9486a2f709
isaki0317 <matsuoisaki.0317@gmail.com>
2020-11-16 06:36:16 +0000

    item new first commit

--- app/controllers/administrator/items_controller.rb
+++ app/controllers/administrator/items_controller.rb
@@ -1,2 +1,30 @@
 class Administrator::ItemsController < ApplicationController
+
+  def new
+    @item_new = Item.new
+  end
+
+  def index
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def create
+    @item = Item.new(item_params)
+    @item.save
+    redirect_to administrator_item_path(@item)
+  end
+
+  def update
+  end
+
+  private
+  def item_params
+    params.require(:item).permit(:name, :discription, :image_id, :genre_id, :price_without_tax, :is_active)
+  end
+
 end
