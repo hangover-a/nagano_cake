d68bef7c87350b95f9717d347dfa1edc51ace9bd
jackie0922youhei <jackie0922youhei@gmail.com>
2020-11-16 05:19:52 +0000

    complete customer's edit page

--- app/controllers/customer/customers_controller.rb
+++ app/controllers/customer/customers_controller.rb
@@ -1,2 +1,18 @@
 class Customer::CustomersController < ApplicationController
+
+  def edit
+    @user = current_customer
+  end
+
+  def update
+    @user = current_customer
+    @user.update(customer_params)
+    redirect_to customers_path
+  end
+
+  private
+  def customer_params
+    params.require(:customer).permit(:email, :last_name, :first_name, :last_name_kana, :first_name_kana, :postcode, :address, :phone_number)
+  end
+
 end
