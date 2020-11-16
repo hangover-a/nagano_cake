0f01879547a1613008ec1936d2644373c32813ad
isaki0317 <matsuoisaki.0317@gmail.com>
2020-11-16 09:33:04 +0000

    genre index

--- app/controllers/administrator/genres_controller.rb
+++ app/controllers/administrator/genres_controller.rb
@@ -1,2 +1,25 @@
 class Administrator::GenresController < ApplicationController
+
+  def index
+    @genre_new = Genre.new
+  end
+
+  def edit
+  end
+
+  def create
+    @genre = Genre.new(genre_params)
+    @genre.save
+    redirect_to administrator_genres_path
+  end
+
+  def update
+  end
+
+  private
+
+  def genre_params
+    params.require(:genre).permit(:name, :is_active)
+  end
+
 end
