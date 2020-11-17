8a63ab404e7eebcd825b97f82d9978d97c44f603
jackie0922youhei <jackie0922youhei@gmail.com>
2020-11-17 04:47:20 +0000

    finished customer's delovery_address_index_page

--- db/migrate/20201112093827_create_addresses.rb
+++ db/migrate/20201112093827_create_addresses.rb
@@ -2,7 +2,7 @@ class CreateAddresses < ActiveRecord::Migration[5.2]
   def change
     create_table :addresses do |t|
 
-      t.integer :costomer_id, null: false
+      t.integer :customer_id, null: false
       t.string :postcode, null: false
       t.string :address, null: false
       t.string :addressee, null: false
