ca407dc807c4cb55493f3f0c9aeddb34581ad62e
jackie0922youhei <jackie0922youhei@gmail.com>
2020-11-17 23:27:59 +0000

    still creating customer's order page

--- app/controllers/customer/orders_controller.rb
+++ app/controllers/customer/orders_controller.rb
@@ -5,7 +5,26 @@ class Customer::OrdersController < ApplicationController
   end
   
   def new
-    
+    @order = Order.new
+    @addresses = Address.where(customer_id: current_customer.id)
+  end
+  
+  def confirm
+    @order = Order.new(order_params)
+    if @order.invalid?
+      render :new
+    end
+  end
+  
+  def create
+    @order = Order.new(order_params)
+    @order.save
+    redirect_to complete_customers_orders_path
+  end
+  
+  private
+  def order_params
+    params.require(:order).permit(:payment_method, :postcode, :address, :addressee)
   end
 
 end
