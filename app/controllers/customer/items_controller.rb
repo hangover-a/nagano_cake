e41b4089b3583d2b55f0f28b2b46737c908c8de8
Ryunosuke Takeichi <takeichi2003@gmail.com>
2020-11-17 07:36:07 +0000

    3

--- app/controllers/customer/items_controller.rb
+++ app/controllers/customer/items_controller.rb
@@ -1,5 +1,11 @@
 class Customer::ItemsController < ApplicationController
     def index
-        @items = Item.all
+     @items = Item.all
+     @genres = Genres.all
+    end
+    
+    def show
+     @item = Item.find(params.id)
+     @genres = Genres.all
     end
 end
