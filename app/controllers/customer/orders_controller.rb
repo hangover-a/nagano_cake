968792e8a2e230e1d1d20a8b934be3bc9b3876e2
jackie0922youhei <jackie0922youhei@gmail.com>
2020-11-17 07:54:08 +0000

    still doing same thing

--- app/controllers/customer/orders_controller.rb
+++ app/controllers/customer/orders_controller.rb
@@ -2,6 +2,7 @@ class Customer::OrdersController < ApplicationController
   
   def index
     @orders = Order.where(customer_id: current_customer.id)
+    @items = OrderedItem.where()
   end
   
 end
