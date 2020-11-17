a82541904f0e4176a1758bdb9ad92f6de94956f1
isaki0317 <matsuoisaki.0317@gmail.com>
2020-11-17 16:08:25 +0000

    cart_item complete

--- app/controllers/customer/cart_items_controller.rb
+++ app/controllers/customer/cart_items_controller.rb
@@ -4,15 +4,36 @@ class Customer::CartItemsController < ApplicationController
   end
 
   def destroy_all
+    @cart_items = current_customer.cart_items.destroy_all
+    redirect_to items_path
   end
 
-  def destroy_all
+  def destroy
+    @cart_item = CartItem.find(params[:id])
+    @cart_item.destroy
+    redirect_to customers_cart_items_path
   end
 
   def create
+    customer_carts = CartItem.find_by(customer_id: current_customer.id)
+
+    unless customer_carts.nil?
+      @cart_item = customer_carts
+    end
+    @cart_item = CartItem.new(cart_item_params)
+    @cart_item.save
+    redirect_to customers_cart_items_path
   end
 
   def update
+    @cart_item = CartItem.find(params[:id])
+    @cart_item.update(cart_item_params)
+    redirect_to customers_cart_items_path
+  end
+
+  private
+  def cart_item_params
+    params.require(:cart_item).permit(:item_id, :quantity, :customer_id)
   end
 
 end
