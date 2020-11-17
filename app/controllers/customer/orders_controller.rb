ccd77b3742e73cbe4999479136743887faded00d
jackie0922youhei <jackie0922youhei@gmail.com>
2020-11-17 09:48:20 +0000

    created customer's order index

--- app/controllers/customer/orders_controller.rb
+++ app/controllers/customer/orders_controller.rb
@@ -1,8 +1,7 @@
 class Customer::OrdersController < ApplicationController
-  
+
   def index
     @orders = Order.where(customer_id: current_customer.id)
-    @items = OrderedItem.where()
   end
-  
+
 end
