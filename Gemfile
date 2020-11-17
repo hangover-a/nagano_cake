06d9b3e61142e7d2152d00d5fc2343c92e494232
isaki0317 <matsuoisaki.0317@gmail.com>
2020-11-17 10:53:19 +0000

    cart_items start

--- Gemfile
+++ Gemfile
@@ -66,3 +66,4 @@ gem 'bootstrap-sass', '~> 3.3.6'
 gem 'pry-rails'
 gem "refile",  require: "refile/rails", github: 'manfe/refile'
 gem "refile-mini_magick"
+gem 'kaminari'
--- app/controllers/customer/cart_items_controller.rb
+++ app/controllers/customer/cart_items_controller.rb
@@ -1,2 +1,18 @@
 class Customer::CartItemsController < ApplicationController
+
+  def index
+  end
+
+  def destroy_all
+  end
+
+  def destroy_all
+  end
+
+  def create
+  end
+
+  def update
+  end
+
 end
