0cfd6b90e231c934ffc5565cda6da11a403035fa
isaki0317 <matsuoisaki.0317@gmail.com>
2020-11-17 01:45:37 +0000

    item new complete

--- app/controllers/administrator/items_controller.rb
+++ app/controllers/administrator/items_controller.rb
@@ -2,6 +2,7 @@ class Administrator::ItemsController < ApplicationController
 
   def new
     @item_new = Item.new
+    @genres = Genre.all
   end
 
   def index
@@ -16,7 +17,7 @@ class Administrator::ItemsController < ApplicationController
   def create
     @item = Item.new(item_params)
     @item.save
-    redirect_to administrator_item_path(@item)
+    redirect_to administrator_items_path
   end
 
   def update
@@ -24,7 +25,7 @@ class Administrator::ItemsController < ApplicationController
 
   private
   def item_params
-    params.require(:item).permit(:name, :discription, :image_id, :genre_id, :price_without_tax, :is_active)
+    params.require(:item).permit(:name, :discription, :image, :genre_id, :price_without_tax, :is_active)
   end
 
 end
