8584392f933ea6fdd4cba1894eca28268e2c08d3
jackie0922youhei <jackie0922youhei@gmail.com>
2020-11-18 21:25:42 +0000

    some changes

--- app/models/order.rb
+++ app/models/order.rb
@@ -2,17 +2,30 @@ class Order < ApplicationRecord
 
   belongs_to :customer
   has_many :ordered_items, dependent: :destroy
-  
+
   with_options presence: true do
     validates :payment_method
     validates :postcode
     validates :address
     validates :addressee
   end
-  
+
   enum payment_method: {
     credit_card: 0,
     bank_transfer: 1
   }
+
+  def price_tax_included(price)
+    (price * 1.1).floor
+  end
+
+  def sub_total(sub)
+    price_tax_included(sub.item.price_without_tax) * sub.quantity
+  end
+
+  def total_cost(totals)
+    cost = 0
+    return cost
+  end
 
 end
