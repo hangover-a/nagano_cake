ec7a3d1a35bb54f47b986e40d3b953c6c0f55b67
Ryunosuke Takeichi <takeichi2003@gmail.com>
2020-11-18 09:24:56 +0000

    third

--- app/models/order.rb
+++ app/models/order.rb
@@ -2,6 +2,7 @@ class Order < ApplicationRecord
 
   belongs_to :customer
   has_many :ordered_items, dependent: :destroy
+  has_many :order_details, dependent: :destroy
   
   with_options presence: true do
     validates :payment_method
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_16_095529) do
+ActiveRecord::Schema.define(version: 2020_11_18_061556) do
 
   create_table "addresses", force: :cascade do |t|
     t.integer "customer_id", null: false
@@ -34,7 +34,7 @@ ActiveRecord::Schema.define(version: 2020_11_16_095529) do
   end
 
   create_table "cart_items", force: :cascade do |t|
-    t.integer "customer_id", null: false
+    t.integer "costomer_id", null: false
     t.integer "item_id", null: false
     t.integer "quantity", null: false
     t.datetime "created_at", null: false
@@ -79,6 +79,16 @@ ActiveRecord::Schema.define(version: 2020_11_16_095529) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "order_details", force: :cascade do |t|
+    t.integer "order_id", null: false
+    t.integer "item_id", null: false
+    t.integer "price", null: false
+    t.integer "amount", null: false
+    t.integer "making_satus", default: 0, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "ordered_items", force: :cascade do |t|
     t.integer "item_id", null: false
     t.integer "order_id", null: false
@@ -90,7 +100,7 @@ ActiveRecord::Schema.define(version: 2020_11_16_095529) do
   end
 
   create_table "orders", force: :cascade do |t|
-    t.integer "customer_id", null: false
+    t.integer "costomer_id", null: false
     t.integer "delivery_fee", null: false
     t.integer "total_cost", null: false
     t.integer "payment_method", default: 0, null: false
