31c1c6cdd8872925c30db996a6a675dd846cad31
jackie0922youhei <jackie0922youhei@gmail.com>
2020-11-18 22:54:51 +0000

    customer's order page things

--- app/models/order.rb
+++ app/models/order.rb
@@ -15,17 +15,4 @@ class Order < ApplicationRecord
     bank_transfer: 1
   }
 
-  def price_tax_included(price)
-    (price * 1.1).floor
-  end
-
-  def sub_total(sub)
-    price_tax_included(sub.item.price_without_tax) * sub.quantity
-  end
-
-  def total_cost(totals)
-    cost = 0
-    return cost
-  end
-
 end
