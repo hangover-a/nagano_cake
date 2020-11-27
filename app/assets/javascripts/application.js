1e475ee00e9b316986d5cc937589bc99909620c4
isaki0317 <matsuoisaki.0317@gmail.com>
2020-11-27 07:40:12 +0000

    slideimage fix

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -28,27 +28,27 @@ $(function() {
   });
 });
 
-$(document).on('turbolinks:load', function () {
-    $("#images").skippr({
-      // スライドショーの変化 ("fade" or "slide")
-      transition : 'slide',
-      // 変化に係る時間(ミリ秒)
-      speed : 4000,
-      // easingの種類
-      easing : 'easeOutQuart',
-      // ナビゲーションの形("block" or "bubble")
-      navType : 'block',
-      // 子要素の種類("div" or "img")
-      childrenElementType : 'div',
-      // ナビゲーション矢印の表示(trueで表示)
-      arrows : true,
-      // スライドショーの自動再生(falseで自動再生なし)
-      autoPlay : true,
-      // 自動再生時のスライド切替間隔(ミリ秒)
-      autoPlayDuration : 6000,
-      // キーボードの矢印キーによるスライド送りの設定(trueで有効)
-      keyboardOnAlways : true,
-      // 一枚目のスライド表示時に戻る矢印を表示するかどうか(falseで非表示)
-      hidePrevious : false
-    });
-  });
+
+var setImage = [
+  "cake_2.jpg",
+  "cake_3.jpg",
+  "cake_4.jpg"];
+
+//カウンターの初期値設定
+var counter = 0;
+
+//設定した画像パスをid="slideshow"のbackground-imageに設定する、繰り返す
+function slideimage(){
+    //カウンターが設定画像数より大きいか確認
+    if( counter >= (setImage.length-1)){
+        //カウンターを初期値に戻す
+        counter = 0;
+    }
+    //背景画像を設定
+    document.getElementById( "slideshow" ).style.backgroundImage = "url(" + setImage[counter] + ")";
+    //カウントアップ
+    counter++;
+}
+
+//3秒ごとに背景画像を切り替える
+setInterval("slideimage()",3000);
