ec0bf32b31e3c6ea78101f8981738a24f015b6fb
isaki0317 <matsuoisaki.0317@gmail.com>
2020-11-28 05:22:45 +0000

    finish image slideshow

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -30,9 +30,10 @@ $(function() {
 
 
 var setImage = [
-  "cake_2.jpg",
-  "cake_3.jpg",
-  "cake_4.jpg"];
+  "cake-2.jpg",
+  "cake-3.jpg",
+  "cake-4.jpg",
+  "cake.jpg"];
 
 //カウンターの初期値設定
 var counter = 0;
@@ -40,12 +41,12 @@ var counter = 0;
 //設定した画像パスをid="slideshow"のbackground-imageに設定する、繰り返す
 function slideimage(){
     //カウンターが設定画像数より大きいか確認
-    if( counter >= (setImage.length-1)){
+    if( counter >= (setImage.length)){
         //カウンターを初期値に戻す
         counter = 0;
     }
     //背景画像を設定
-    document.getElementById( "slideshow" ).style.backgroundImage = "url(" + setImage[counter] + ")";
+    document.getElementById( "slideimage" ).style.backgroundImage = "url(" + setImage[counter] + ")";
     //カウントアップ
     counter++;
 }
